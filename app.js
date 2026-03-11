28672494538e8f32b0fe87d11f1089c4b7ce48e4
RehamHussam <rehamhussam51@gmail.com>
2026-03-11 17:13:36 -0400

    Update message and use secrets

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Custom Build Environment!' });
+  res.json({ message: 'Hello from Custom Build Environment with Secrets!' });
 });
 
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -5,5 +5,5 @@ test('Hello endpoint returns correct message', async () => {
   const res = await request(app).get('/');
   
   expect(res.statusCode).toBe(200);
-  expect(res.body).toEqual({ message: 'Hello from Custom Build Environment!' });
+  expect(res.body).toEqual({ message: 'Hello from Custom Build Environment with Secrets!' });
 });
